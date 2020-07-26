70fc73475c8e8d43a8c365acd73748bc79c21898
John-Cham <chamj@mskcc.org>
2020-07-25 21:12:56 -0400

    figured out how to make the for loop work

--- Reading_Files.py
+++ Reading_Files.py
@@ -4,28 +4,12 @@ os.chdir('/Users/John/Downloads/')
 
 def List_Maker(string): 
     li = list(string.split("\t"))
-    return li 
+    return li
 
-#I tried to make this a for loop but it would only out put every other hugo symbol 
+#never mind i figure out how to print the Hugo symbols
 with open('test_file.txt', 'r') as reader:
-#   for line in reader:   
     x= List_Maker(reader.readline())
-    print(x[0])
-    
-    x= List_Maker(reader.readline())
-    print(x[0])
-    
-    x= List_Maker(reader.readline())
-    print(x[0])
-    
-    x= List_Maker(reader.readline())
-    print(x[0])
-    
-    x= List_Maker(reader.readline())
-    print(x[0])
-    
-    x= List_Maker(reader.readline())
-    print(x[0])
-    
-    x= List_Maker(reader.readline())
-    print(x[0])
+    for i in x: 
+            x= List_Maker(reader.readline())   
+            print(x[0])
+        
