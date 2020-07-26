2f810fa923dc141d71a39d3e0a67c2b7adfefbe7
John-Cham <67881602+John-Cham@users.noreply.github.com>
2020-07-25 22:17:47 -0400

    Update File_Dictionary.py

--- File_Dictionary.py
+++ File_Dictionary.py
@@ -19,13 +19,3 @@ with open('test_file.txt', 'r') as reader:
 #gonna work some more on this tomorrow I know that this is incorrect. I cannot seem to access the values using the keys. It should just be one dictionary but the for loop makes it multiple
 
 
-#fruits = ["Apple", "Pear", "Peach", "Banana"]
-#prices = [0.35, 0.40, 0.40, 0.28]
-
-#fruit_dictionary = dict(zip(fruits, prices))
-
-#print(fruit_dictionary)
-#Remove the first element (Hugo Symbol) from the arrayadd the Hugo symbol as the key
-#and the new list as the value.
-
-#maybe I make two lists make these outputs a list and make the dictionary of values a seperate list 
