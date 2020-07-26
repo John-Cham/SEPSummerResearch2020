3d116801eadbeddb0c0ab500405878c9594fd885
John-Cham <67881602+John-Cham@users.noreply.github.com>
2020-07-25 21:13:38 -0400

    Update Reading_Files.py

--- Reading_Files.py
+++ Reading_Files.py
@@ -6,7 +6,7 @@ def List_Maker(string):
     li = list(string.split("\t"))
     return li
 
-#never mind i figure out how to print the Hugo symbols
+
 with open('test_file.txt', 'r') as reader:
     x= List_Maker(reader.readline())
     for i in x: 
