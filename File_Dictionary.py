1b786f78cc875b6f2ef9c72ddb8d85182856fc7c
John-Cham <chamj@mskcc.org>
2020-07-27 22:02:33 -0400

    'fixed'

--- File_Dictionary.py
+++ File_Dictionary.py
@@ -3,16 +3,15 @@ import os
 os.chdir('/Users/John/Downloads/')
 
 
-meth_dict= {}
 
+meth_dict= {}
 with open('test_file.txt', 'r') as reader:
     next(reader) 
     for line in reader:
         meth_data= line.strip().split('\t')    
         hugo_symbol= meth_data[0]
         meth_data.pop(0)
-        
-        meth_dict={hugo_symbol:meth_data}
-        print (meth_dict)
+        meth_dict[hugo_symbol]=meth_data
+print (meth_dict)
            
 
